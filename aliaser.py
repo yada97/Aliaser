bb60241b801962de967527b255836be3e439eb9c
michael <sosobrook@protonmail.com>
2021-09-01 12:40:32 +0300

    complete features

--- aliaser.py
+++ aliaser.py
@@ -1,6 +1,12 @@
 #! /usr/bin/python3
 
 #Built for Orion Group Simple scripts for daily use
+"""
+If you are using bash change the following lines to [.bashrc] 47, 51, 64 ,65
+Built for Orion group pair programmers
+
+
+"""
 
 import time
 import sys
@@ -33,14 +39,16 @@ def art():
 def alias_workout(w_path, process, delay, repeat):
     for path in pathlib.Path(w_path).iterdir():
         if path.is_dir():
+            os.chdir(w_path)
             chosen_path = path.stem
-            src = stress_location + "/"+ chosen_path
-            alias = "alias " +chosen_path+"="+ "\"" +stress_location+"/"+chosen_path+ "\""
+            #src = stress_location + "/"+ chosen_path
+            alias = "alias " +chosen_path+"="+ "\"" +w_path+"/"+chosen_path+ "\""
 
             #Main program writes to .zshrc and stores your aliase ther
             f = open(home + ".zshrc", "a")
             f.write(alias + "\n")
             f.close()
+            os.system("zsh")
             #Temporary files will be used when the need to delete a folder arises
             f = open(home + ".alias_catalog", "a")
             f.write(alias +"\n")
@@ -70,18 +78,24 @@ def Main():
     if args.path:
         w_path = args.path
         t1 = Thread(target=alias_workout, args=(w_path, "process_1", 1,2))
-        t2 = Thread(target=alias_workout, args=(w_path, "process_2", 1,3))
+        #t2 = Thread(target=alias_workout, args=(w_path, "process_2", 1,3))
         t1.start()
-        t2.start()
+        #t2.start()
     elif args.auto:
-        w_path = os.getcwd()
-        t1 = Thread(target=alias_workout, args=(w_path, "process_1", 1,2))
-        t2 = Thread(target=alias_workout, args=(w_path, "process_2", 1,3))
-        t1.start()
-        t2.start()
+        if args.auto == "auto":
+            w_path = os.getcwd()
+            t1 = Thread(target=alias_workout, args=(w_path, "process_1", 1,2))
+            #t2 = Thread(target=alias_workout, args=(w_path, "process_2", 1,3))
+            t1.start()
+            #t2.start()
+        else:
+            print(f"{Fore.RED} Usage ./aliaser.py -a auto, For current directory")
     elif args.r :
-        reset_alias()
-        os.system("zsh")
+        if args.r == "reset":
+            reset_alias()
+            os.system("zsh")
+        else:
+            print(f"{Fore.RED} Usage ./aliaser.py -r reset, To reset config to Default")
     else:
         os.system("clear")
         art()
